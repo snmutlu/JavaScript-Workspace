4ced8fd3c018492e3fbcda780fc5ecdc3174b262
Mutlu SENEM <info.mutlusenem@gmail.com>
2023-04-07 18:36:41 +0300

    javascript

---
+++ Temeller/D-2-Console/app.js
@@ -0,0 +1,16 @@
+// console.log("Merhaba Javascript");
+// console.log(212312);
+// console.log(true);
+// console.log(false);
+
+// var a = 20;
+
+// console.log(a);
+
+// console.log(["Ahmet""Mehmet"]);
+
+// console.log(typeof a);
+
+console.warn("Bu bir uyarıdır.");
+console.error("Bu sayfa bulunamadı");
+console.clear();
---
+++ Temeller/D-3-Değişkenler, VeriTipleri/app.js
@@ -0,0 +1,92 @@
+//Değişken Oluşturma
+
+// var a = 10;
+// var b = 20;
+// var c = 40;
+
+// console.log(a,b,c);
+
+//Primitive Veri Tipleri (ilkel veri tipleri)
+
+// var a = 10; //Number Veri Tipi
+// var b = 3.14; //Number Veri Tipi
+// console.log(typeof a);
+// console.log(typeof b);
+
+
+//String
+
+// var name = "Mustafa";
+// console.log(name);
+// console.log(typeof name);
+
+//Boolean
+
+// var a = true;
+// console.log(typeof a);
+
+//Null
+
+// var a = null;
+// console.log(a);
+// console.log(typeof a);
+
+//Undefined
+
+// var a;
+// console.log(a);
+
+
+
+
+
+
+
+//Referance Veri Tipleri
+
+//Array
+// var numbers = [1,2,3,4,5];
+// console.log(numbers);
+// console.log(typeof numbers);
+// console.log(numbers[0]);
+
+// var person = {
+//     name:"Mustafa Murat",
+//     age : 25
+// }
+// console.log(person);
+// console.log(typeof person);
+
+
+// var date = new Date();
+// console.log(date);
+// console.log(typeof date);
+
+
+// var merhaba = function(){
+//     console.log("Merhaba");
+// }
+// console.log(merhaba)
+// console.log(typeof merhaba);
+
+// var a = 10;
+// var b = a;
+// console.log(a,b);
+// a = 20;
+// console.log(a,b);
+
+var a = [1,2,3];
+
+var b = a;
+
+a.push(4);
+console.log(b);
+
+
+
+
+
+
+
+
+
