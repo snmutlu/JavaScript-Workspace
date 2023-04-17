1563be4d3ac8b1affaa7714a6c5ee7803d254685
Mutlu SENEM <info.mutlusenem@gmail.com>
2023-04-18 01:45:02 +0300

    update

--- Temeller/D-4-Değişken Tanımlama (Var-Let-Const Farkları)/app.js
+++ Temeller/D-4-Değişken Tanımlama (Var-Let-Const Farkları)/app.js
@@ -1,5 +1,74 @@
 // var
 
 
-var a;
-console.log(a);
+// var a;
+// console.log(a);
+
+// var name = "Mustafa Murat";
+
+// var name = "Murat";
+
+// console.log(name);
+
+// var name = "Mustafa Murat";
+// var name = "Murat";
+// console.log(name);
+
+// Var anahtar kelimesiyle atadığımız değerleri yukarıdaki gibi sonraki
+//satırlarda değiştirebiliyoruz.
+
+// Let
+
+// let name = "Mustafa Murat";
+
+// console.log(name);
+
+// name = "Murat";
+
+// console.log(name);
+
+// let ile var arasındaki fark
+
+// let name = "Mustafa Murat";
+// let name = "Murat";
+
+//Yukarıdaki şekilde yazdığımız zaman let anahtar kelimesiyle var' dan farklı olarak hata verir
+
+// let a,b;
+
+// a=10;
+// b=20;
+
+// console.log(a+b);
+
+//Const Yani Constant (Sabit Anlamına Geliyor)
+//Yani biz bir değişkenin değerini ilk başta verdiğimiz zaman başka herhangi
+//bir yerde değiştiremiyoruz.
+
+//Örn;
+
+// const name = "Mustafa Murat";
+// console.log(name);
+
+// name = "Murat";
+// console.log(name);
+
+// Bu şekilde yazdığımız için bir Assignment to constant variable. hatası alırız
+
+// const a;
+// a=10;
+// console.log(a);
+
+// Bu şekilde ise a nın değerini direk const a; bölümünde vermediğimiz için
+// Missing initializer in const declaration hatası alırız.
+
+// Biz const kullanırken yeni değer ataması yaptığımızda hata alırız ancak
+// yeni değer eklediğimiz zaman herhangi bir hata almayız
+
+// Örn;
+
+// const a = [1,2,3,4,5];
+// console.log(a);
+
+// a.push(6);
+// console.log(a);
